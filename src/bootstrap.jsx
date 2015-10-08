7e6215b77a668d5bb63c7f43f7dca2208627a7e9
Andris Silis <andris.silis@gmail.com>
2015-10-08 08:21:30 +0300

    Tick moved to separate module

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -10,48 +10,10 @@ import initialState from "./initialState";
 import * as actionCreators from "./actions/actionCreators";
 import { listenToKeys } from "./keyboardController";
 import megaReducer from "./megaReducer";
+import tick from "./tick";
 
 const store = createStore(megaReducer, fromJS(initialState));
 window.store = store;
-
-
-function tick(store) {
-    const dispatch = store.dispatch;
-    let state = store.getState();
-
-    // Create next tick
-    setTimeout(() => { tick(store) }, state.get("speed"));
-
-    if (!state.get("isGameStarted") || state.get("isGamePaused")) {
-        return;
-    }
-
-    let snakeBody = state.get("snakeBody"),
-        nextPosition = snakeUtil.getNextPosition(
-            snakeBody.last(),
-            state.get("direction")
-        ),
-        foodPosition = state.get("foodPosition");
-
-    // might be useful to have more generic actions, such as - prepareForNextTick
-    dispatch(actionCreators.resetDirectionBufferFlag());
-
-    if (snakeUtil.positionsMatch(nextPosition, foodPosition)) {
-        dispatch(actionCreators.increaseScore());
-        dispatch(actionCreators.increaseSpeed());
-        dispatch(actionCreators.grow());
-        dispatch(actionCreators.spawnFood());
-        dispatch(actionCreators.decreaseCameraOffsetZ());
-    } else {
-        dispatch(actionCreators.move());
-    }
-
-    if (state.get("bufferedDirection")) {
-        dispatch(actionCreators.changeDirection(state.get("bufferedDirection")));
-        dispatch(actionCreators.bufferDirection(null));
-    }
-}
-
 
 listenToKeys(store);
 tick(store);
---
+++ src/tick.js
@@ -0,0 +1,36 @@
+export default function (store) {
+  const dispatch = store.dispatch;
+  let state = store.getState();
+
+  // Create next tick
+  setTimeout(() => { tick(store) }, state.get("speed"));
+
+  if (!state.get("isGameStarted") || state.get("isGamePaused")) {
+    return;
+  }
+
+  let snakeBody = state.get("snakeBody"),
+    nextPosition = snakeUtil.getNextPosition(
+      snakeBody.last(),
+      state.get("direction")
+    ),
+    foodPosition = state.get("foodPosition");
+
+  // might be useful to have more generic actions, such as - prepareForNextTick
+  dispatch(actionCreators.resetDirectionBufferFlag());
+
+  if (snakeUtil.positionsMatch(nextPosition, foodPosition)) {
+    dispatch(actionCreators.increaseScore());
+    dispatch(actionCreators.increaseSpeed());
+    dispatch(actionCreators.grow());
+    dispatch(actionCreators.spawnFood());
+    dispatch(actionCreators.decreaseCameraOffsetZ());
+  } else {
+    dispatch(actionCreators.move());
+  }
+
+  if (state.get("bufferedDirection")) {
+    dispatch(actionCreators.changeDirection(state.get("bufferedDirection")));
+    dispatch(actionCreators.bufferDirection(null));
+  }
+}
