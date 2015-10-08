1fe23e9392d432895f7f3f184854c1f572357ea2
Andris Silis <andris.silis@gmail.com>
2015-10-08 10:29:28 +0300

    Formatting

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(require("webpack-dev-middleware")(compiler, {
 
 app.use(require("webpack-hot-middleware")(compiler));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
@@ -27,26 +27,24 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 server.listen(3000, "localhost", function (err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-  console.log("Listening at http://localhost:3000");
+    console.log("Listening at http://localhost:3000");
 });
 
 
-
-io.on("connection", function(client) {
+io.on("connection", function (client) {
     console.log("Client connected...");
 
-    client.on("join", function(data) {
+    client.on("join", function (data) {
         console.log("Client joined", data);
     });
 
-    client.on("snakeBody", function(data) {
+    client.on("snakeBody", function (data) {
         console.log("snakeBody received", data);
     });
 
 });
-
