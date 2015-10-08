fd70109b0d9a7dbeca5ec95c0c574a08037042b9
Andris Silis <andris.silis@gmail.com>
2015-10-08 08:21:12 +0300

    Central reducer moved to separate module

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -9,55 +9,9 @@ import * as snakeUtil from "./utils/snakeUtil";
 import initialState from "./initialState";
 import * as actionCreators from "./actions/actionCreators";
 import { listenToKeys } from "./keyboardController";
+import megaReducer from "./megaReducer";
 
-const mergedReducers = (state, action) => {
-    state = reducer.initGame(state, action);
-
-    state = reducer.spawnFood(state, action);
-
-    state = reducer.grow(state, action);
-
-    state = reducer.startGame(state, action);
-
-    state = state.set(
-        "isGamePaused",
-        reducer.pauseGame(state.get("isGamePaused"), action)
-    );
-
-    state = reducer.move(state, action);
-
-    state = reducer.changeDirection(state, action);
-
-    state = state.set(
-        "bufferedDirection",
-        reducer.bufferDirection(state.get("bufferedDirection"), action)
-    );
-
-    state = state.set(
-        "directionChangedInTick",
-        reducer.resetDirectionBufferFlag(state.get("directionChangedInTick"), action)
-    );
-
-    state = state.set(
-        "score",
-        reducer.increaseScore(state.get("score"), action)
-    );
-
-    state = state.set(
-        "speed",
-        reducer.increaseSpeed(state.get("speed"), action)
-    );
-
-    state = state.set(
-        "cameraOffsetZ",
-        reducer.decreaseCameraOffsetZ(state.get("cameraOffsetZ"), action)
-    );
-
-    return state;
-};
-
-
-const store = createStore(mergedReducers, fromJS(initialState));
+const store = createStore(megaReducer, fromJS(initialState));
 window.store = store;
 
 
