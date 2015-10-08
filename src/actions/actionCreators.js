92ed3613b1122c7e6024b4fd1650e7ee865bc7b9
Andris Silis <andris.silis@gmail.com>
2015-10-08 09:19:25 +0300

    Code format

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -3,40 +3,40 @@ import * as consts from "./actionTypes";
 export function move() {
 	return {
 		type: consts.MOVE
-	}
+	};
 }
 
 export function changeDirection(direction) {
 	return {
 		type: consts.CHANGE_DIRECTION,
 		direction
-	}
+	};
 }
 
 export function bufferDirection(direction) {
 	return {
 		type: consts.BUFFER_DIRECTION,
 		direction
-	}
+	};
 }
 
 export function resetDirectionBufferFlag(direction) {
 	return {
 		type: consts.RESET_DIRECTION_BUFFER_FLAG,
 		direction
-	}
+	};
 }
 
 export function die() {
 	return {
 		type: consts.DIE
-	}
+	};
 }
 
 export function grow() {
 	return {
 		type: consts.GROW
-	}
+	};
 }
 
 
@@ -49,38 +49,38 @@ export function initGame() {
 export function startGame() {
 	return {
 		type: consts.START_GAME
-	}
+	};
 }
 
 export function pauseGame() {
 	return {
 		type: consts.PAUSE_GAME
-	}
+	};
 }
 
 export function increaseScore(amount=1) {
 	return {
 		type: consts.INCREASE_SCORE,
         amount
-	}
+	};
 }
 
 export function increaseSpeed(amount=5) {
 	return {
 		type: consts.INCREASE_SPEED,
         amount
-	}
+	};
 }
 
 export function spawnFood() {
 	return {
 		type: consts.SPAWN_FOOD
-	}
+	};
 }
 
 export function decreaseCameraOffsetZ(amount=0.5) {
 	return {
 		type: consts.DECREASE_CAMERA_Z,
 		amount
-	}
+	};
 }
--- src/initialState.js
+++ src/initialState.js
@@ -2,19 +2,19 @@ import { Map } from "immutable";
 import * as snakeUtil from "./utils/snakeUtil";
 
 export default {
-	direction: "up",
-	bufferedDirection: null, // used for better handling when multiple direction keystrokes are made in one tick
-	snakeBody: snakeUtil.addMultipleSegments(
-		{ x: 0, y: 0},
-		"up",
-		5
-	),
-	speed: 100, // ms per tick
-	isGameStarted: false, // false when just initialized
-	isGamePaused: false,
+    direction: "up",
+    bufferedDirection: null, // used for better handling when multiple direction keystrokes are made in one tick
+    snakeBody: snakeUtil.addMultipleSegments(
+        {x: 0, y: 0},
+        "up",
+        5
+    ),
+    speed: 100, // ms per tick
+    isGameStarted: false, // false when just initialized
+    isGamePaused: false,
     score: 0,
-	foodPosition: { x: 0, y: 0 },
-	gridSize: { width: 40, height: 40 },
-	cameraOffsetZ: 30,
-	directionChangedInTick: false
+    foodPosition: {x: 0, y: 0},
+    gridSize: {width: 40, height: 40},
+    cameraOffsetZ: 30,
+    directionChangedInTick: false
 };
