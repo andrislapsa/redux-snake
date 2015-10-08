7f476ffde1c3b92dcbd42586ca085a66cfe120be
Andris Silis <andris.silis@gmail.com>
2015-10-08 09:35:05 +0300

    Single quotes to double quotes

--- server.js
+++ server.js
@@ -1,29 +1,46 @@
-var path = require('path');
-var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config');
+var path = require("path");
+var express = require("express");
+
+var webpack = require("webpack");
+var config = require("./webpack.config");
 
 config.entry.unshift("webpack-hot-middleware/client");
 
 var app = express();
 var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require("webpack-hot-middleware")(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, 'localhost', function (err) {
+
+var expressWsApp = expressWs(app);
+app.ws("/", function(ws, req) {
+    ws.on("message", function(msg) {
+        console.log(msg);
+    });
+    console.log("socket", req.testing);
+});
+
+
+app.listen(3000, "localhost", function (err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log("Listening at http://localhost:3000");
 });
+
+
+
+
+
